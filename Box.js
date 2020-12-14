3f9b605016809d31fa27d0584a70282d78bc44ad
MuditKaushik1810 <69709465+MuditKaushik1810@users.noreply.github.com>
2020-12-14 17:26:48 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -9,8 +9,11 @@ class Box{
         this.width = width;
         this.height = height;
         World.add(world, this.body);
+        this.visibility = 255;
       }
       display(){
+        if (this.body.speed<3){
+          
         var angle = this.body.angle;
         push();
         translate(this.body.position.x, this.body.position.y);
@@ -18,5 +21,22 @@ class Box{
         rectMode(CENTER);
         rect( 0, 0, this.width, this.height);
         pop();
+
+      }
+      else{
+      World.remove(world, this.body); 
+      push();
+      this.visibility = this.visibility-5; 
+      tint(255,this.visibility);
+      rect(this.body.position.x,this.body.position.y,50,50);
+
+      
+      pop();
+      }
+      }
+      score (){
+        if(this.Visiblity < 0 && this.Visiblity > -105){
+          score++
+        }
       }
 }
--- sketch.js
+++ sketch.js
@@ -4,9 +4,11 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-
+var score = 0;
+var backgroundImg;
 function preload(){
 polygon_img = loadImage("polygon.png")
+getBackgroundImg();
 }
 
 function setup(){
@@ -19,38 +21,63 @@ function setup(){
     platform2 = new Ground(700,200,200,10);
     //level 1
     block1 = new Box(300,275,30,40);
+    block1.score();
     block2 = new Box(330,275,30,40);
+    block2.score();
     block3 = new Box(360,275,30,40);
+    block3.score();
     block4 = new Box(390,275,30,40);
+    block4.score();
     block5 = new Box(420,275,30,40);
+    block5.score();
     block6 = new Box(450,275,30,40);
+    block6.score();
     block7 = new Box(480,275,30,40);
+    block7.score();
     //level2
     block8 = new Box(330,235,30,40);
+    block8.score();
     block9 = new Box(360,235,30,40);
+    block9.score();
     block10 = new Box(390,235,30,40);
+    block10.score();
     block11= new Box(420,235,30,40);
+    block11.score();
     block12 = new Box(450,235,30,40);
+    block12.score();
     //level3
     block13 = new Box(360,195,30,40);
+    block13.score();
     block14 = new Box(390,195,30,40);
+    block14.score();
     block15 = new Box(420,195,30,40);
+    block15.score();
     //toplevel
     block16 = new Box(390,155,30,40);
+    block16.score();
 
     //set 2 for second stand
     //level one
     blocks1 = new Box(640,175,30,40);
+    blocks1.score();
     blocks2 = new Box(670,175,30,40);
+    blocks2.score();
     blocks3 = new Box(700,175,30,40);
+    blocks3.score();
     blocks4 = new Box(730,175,30,40);
+    blocks4.score();
     blocks5 = new Box(760,175,30,40);
+    blocks5.score();
     //level two
     blocks6 = new Box(670,135,30,40);
+    blocks6.score();
     blocks7 = new Box(700,135,30,40);
+    blocks7.score();
     blocks8 = new Box(730,135,30,40);
+    blocks8.score();
     //top
     blocks9 = new Box(700,95,30,40);
+    blocks9.score();
 
     polygon = Bodies.circle(50,200,20);
     World.add(world,polygon);
@@ -63,8 +90,16 @@ function setup(){
 }
 
  function draw(){
-    background(170);  
+  if(backgroundImg)
+  background(backgroundImg);  
+  
+  //background(170);  
     Engine.update(engine); 
+    
+    noStroke()
+    textSize(35)
+    fill("white")
+    text("SCORE: " + score, width-300, 50)
     
     textSize(20);
     fill("lightyellow");
@@ -108,8 +143,11 @@ function setup(){
     fill("pink")
     blocks9.display();
     fill("gold");
-    imageMode(CENTER)
-    image(polygon_img ,polygon.position.x,polygon.position.y,40,40);
+    //imageMode(CENTER)
+    ellipseMode(CENTER)
+
+    //image(polygon_img ,polygon.position.x,polygon.position.y,40,40);
+    circle(polygon.position.x,polygon.position.y,40);
     slingshot.display();
     //drawSprites();
  }  
@@ -118,8 +156,48 @@ function setup(){
     Matter.Body.setPosition(this.polygon,{x:mouseX,y:mouseY});
   }
   function mouseReleased(){
-    SlingShot.fly();
+    slingshot.fly();
+    
   }
 
+function keyPressed(){
+  if(keyCode === 32){
+    slingshot.attach(this.polygon)
+  }
+}
 
+async function getBackgroundImg () {
+ var response  = await fetch ("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+ var  responseJSON = await response.json();
+ var time = responseJSON.datetime
+ var hour  = time.slice(11,13);
+ console.log(hour);
+
+ if (hour>=06 && hour<=18){
+      bg = "lightBg.jpg"
+ }
+ else{
+   bg = "darkBg.jpeg"
+ }
+ backgroundImg = loadImage("lightBg.jpg")
+}
                                                                   
+/*
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    var responseJSON = await response.json();
+    var time = responseJSON.datetime
+    var hour = time.slice(11,13);
+    console.log(hour);
+
+    if(hour>=06 && hour<=19){
+        bg = "sprites/bg.png"
+    }
+    else{
+        bg = "sprites/bg2.jpg"  
+    }
+    backgroundImg = loadImage(bg);
+}
+
+
+*/
